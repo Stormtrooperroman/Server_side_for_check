d54e020de3c6cb7ddd7f8685f61cea8382d43fe2
Stormtrooperroman <44937524+Stormtrooperroman@users.noreply.github.com>
2022-06-07 21:27:27 +0700

    Update settings.py

--- FreeZone/settings.py
+++ FreeZone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u)nar*#ysm_h%k$9_#71$=x@f971^-cicq*uf=axksi9g20op2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
